44b1727f81abec5c2beb9b78b2b8b66703d03b64
corno <corno.astn.a@schraverus.com>
2022-06-30 21:54:56 +0200

    .

--- lib/src/_TMPGlobals.ts
+++
@@ -1,4 +0,0 @@
-interface Array<T> {
-
-    length: number
-}
--- lib/src/index.ts
+++ lib/src/index.ts
@@ -1,5 +1,2 @@
 
-export * from "./etc/createCodeCompletionFinder"
-export * from "./etc/createCodeCompletionsGenerator"
-export * from "./etc/createHoverTextFinder"
-export * from "./etc/createHoverTextsGenerator"
+export * from "./etc/process"
