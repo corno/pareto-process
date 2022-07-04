134f709b21d302cb73757a0ff79923d2765158e0
corno <corno.astn.a@schraverus.com>
2022-07-04 14:16:05 +0200

    .

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,7 +5,7 @@ export type Call_Data = string
 
 export type Call_Interface<T> = {
     onResult: (stdout: string) => asyncAPI.IAsync<T>,
-    onError: (stderr: string, exitCode: number) => asyncAPI.IAsync<T>,
+    onError: (stderr: string, exitCode?: number) => asyncAPI.IAsync<T>,
 }
 
 export type Call_ReturnValue<T> = asyncAPI.IAsync<T> 
