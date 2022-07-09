d4184f7e9f5804ae159750835bc253d970f1aed2
corno <corno.astn.a@schraverus.com>
2022-07-09 15:52:48 +0200

    .

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,14 @@
-import * as asyncAPI from "pareto-async-api"
+import * as pa from "pareto-api-core"
 
 
 export type Call_Data = string
 
 export type Call_Interface<T> = {
-    onResult: (stdout: string) => asyncAPI.IAsync<T>,
-    onError: (stderr: string, exitCode?: number) => asyncAPI.IAsync<T>,
+    onResult: (stdout: string) => pa.IAsync<T>,
+    onError: (stderr: string, exitCode?: number) => pa.IAsync<T>,
 }
 
-export type Call_ReturnValue<T> = asyncAPI.IAsync<T> 
+export type Call_ReturnValue<T> = pa.IAsync<T> 
 
 export type Call = <T>(
     $d: Call_Data,
